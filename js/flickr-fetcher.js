6c68a4bb885f8cc0fa85fb880291cf59ea3b9d7e
Niall Slater <niallslater@gmail.com>
2018-08-14 14:20:18 +0100

    URL testing works

--- js/flickr-fetcher.js
+++ js/flickr-fetcher.js
@@ -1,9 +1,16 @@
 var FlickrFetcher;
 
 FlickrFetcher = {
-    photoObjToURL: function() {
-        return 'https://farm2.staticflickr.com/1577/24770505034_31a9986429_b.jpg';
+    photoObjToURL: function(photoObj) {
+        return [ 'https://farm',
+            photoObj.farm, '.staticflickr.com/',
+            photoObj.server, '/',
+            photoObj.id, '_',
+            photoObj.secret, '_b.jpg'
+        ].join('');
     }
 };
 
 module.exports = FlickrFetcher;
+
+//https://jrsinclair.com/articles/2016/gentle-introduction-to-javascript-tdd-intro/
