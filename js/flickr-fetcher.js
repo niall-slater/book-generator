b9b9e188e723c133972a4351fbb9692ddb92c56f
Niall Slater <niallslater@gmail.com>
2018-08-15 11:40:49 +0100

    Tests in place

--- js/flickr-fetcher.js
+++ js/flickr-fetcher.js
@@ -2,12 +2,19 @@ var FlickrFetcher;
 
 FlickrFetcher = {
     photoObjToURL: function(photoObj) {
-        return [ 'https://farm',
+        return ['https://farm',
             photoObj.farm, '.staticflickr.com/',
             photoObj.server, '/',
             photoObj.id, '_',
             photoObj.secret, '_b.jpg'
         ].join('');
+    },
+    
+    transformPhotoObj: function(photoObj) {
+        return {
+            title: photoObj.title,
+            url:   FlickrFetcher.photoObjToURL(photoObj)
+        }
     }
 };
 
