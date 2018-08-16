e73bed21b2513e14e0865b808118875eedb7c79b
Niall Slater <niallslater@gmail.com>
2018-08-16 13:51:31 +0100

    Fixed missing JQuery

--- js/photo-lister.js
+++ js/photo-lister.js
@@ -27,3 +27,8 @@ if ((typeof module !== 'undefined') && (typeof module.exports !== 'undefined')) 
     module.exports = PhotoLister;
 }
 
+FlickrFetcher.fetchPhotos('8060d4cdac3ceb86af470aae29af3a56')
+    .then(PhotoLister.photoListToHTML)
+    .then(function(photosHTML) {
+        PhotoLister.addPhotosToElement($, '#mydiv', photosHTML);
+    });
